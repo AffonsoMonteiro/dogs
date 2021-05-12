bd7e7f96037c90cc6c6080bff4d56a3da10e6d4c
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-05-11 22:04:18 -0300

    create header

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            footer
+        </div>
+    )
+}
+
+export default Footer
