303f17c106eb2814053538d8cef3e3c281010295
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-05-12 23:09:44 -0300

    add form and regex

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import LoginCreate from './LoginCreate'
+import LoginForm from './LoginForm'
+import LoginPasswordLost from './LoginPasswordLost copy'
+import LoginPasswordReset from './LoginPasswordReset'
 
 const Login = () => {
     return (
         <div>
-            login
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="/criar" element={<LoginCreate />} />
+                <Route path="/perdeu" element={<LoginPasswordLost />} />
+                <Route path="/resetar" element={<LoginPasswordReset />} />
+            </Routes>
         </div>
     )
 }
---
+++ src/Components/Login/LoginCreate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginCreate = () => {
+    return (
+        <div>
+            login const
+            
+        </div>
+    )
+}
+
+export default LoginCreate
