ca75a37fba6f21ced8123b244303f86f239cc6bf
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-06-03 17:58:45 -0300

    add userProfile

---
+++ src/Components/User/UserProfile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import Feed from '../Feed/Feed'
+
+const UserProfile = () => {
+
+    const { user } = useParams()
+
+    return (
+        <section className="container mainSection">
+            <h1 className="title"> {user} </h1>
+            <Feed />
+        </section>
+    )
+}
+
+export default UserProfile
