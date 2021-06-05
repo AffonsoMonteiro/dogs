82c44fc029760f71fa4e080818b5cac59c8d2589
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-06-05 13:02:07 -0300

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs-footer.svg'
 
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return  (
+  
+  <div className={styles.footer}>
+    
+  <Dogs />
+
+  <p>Alguns direitos reservados.</p>
+
+  </div>
+  )
 };
 
 export default Footer;
